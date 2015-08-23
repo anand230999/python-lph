afbbc98355214978c213f475bb2157df34c0e65b
Anand Krishnan <anand230999@gmail.com>
2015-08-23 19:53:14 +0530

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# a comment this soyou can read your program later
+# anything after the # is ignored by python.
+
+print "i could have code like this" #and the comment after is ignored
+
+# you can also use comment to "disable" or comment out a piece of code:
+# print "this won't run."
+
+print "this will run"
