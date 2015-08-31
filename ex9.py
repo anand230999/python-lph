c6d7be5019eb271d74b456dfb6968ea1fff073a6
Anand Krishnan <anand230999@gmail.com>
2015-08-31 16:53:53 +0530

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days="mon tue wed thr sat sun"
+months="jan\nfeb\nmar\napr\nmay\njun\njul\naug"
+
+print "Here are the days:", days
+print "Here are the months:",months
+
+print """
+There's something going on here
+with the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5,or6.
+""" 
